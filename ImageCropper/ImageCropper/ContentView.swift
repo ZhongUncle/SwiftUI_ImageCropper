9dafdab9d6902da53ef50baf10931624dc881567
ZhongUncle <2020738339@qq.com>
2022-01-20 08:14:21 +0800

    Create the new project

---
+++ ImageCropper/ImageCropper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCropper
+//
+//  Created by 钟宜江 on 2022/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageCropper/ImageCropper/ImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperApp.swift
+//  ImageCropper
+//
+//  Created by 钟宜江 on 2022/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
