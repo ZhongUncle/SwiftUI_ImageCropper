234625b4bee705b48cc4c98abe1819acd85d6f22
ZhongUncle <2020738339@qq.com>
2022-11-24 04:27:05 +0800

    修改了NavigationView被废弃导致的的bug，并且删除了一些没用的文件

--- ImageCropper/ImageCropper/ContentView.swift
+++ ImageCropper/ImageCropper/ContentView.swift
@@ -12,33 +12,69 @@ struct ContentView: View {
     @State private var croppedImage: UIImage = UIImage()
     @State private var showCropper = false
     var body: some View {
-        NavigationView {
-            ZStack {
-                // This will navigate into cropper view
-                NavigationLink(destination: CropperView(inputImage: inputimage, croppedImage: $croppedImage),isActive: $showCropper) { EmptyView() }
-                
-                VStack {
-                    Text("Origin Image")
-                    Image(uiImage: inputimage)
-                        .resizable()
-                        .scaledToFit()
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                ZStack {
+                    // This will navigate into cropper view
+                    NavigationLink(destination: CropperView(inputImage: inputimage, croppedImage: $croppedImage),isActive: $showCropper) { EmptyView() }
                     
-                    Text("Cropped Image")
-                    Image(uiImage: croppedImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: screenWidth * 0.6, height: screenHeight/5)
+                    VStack {
+                        Text("Origin Image")
+                        Image(uiImage: inputimage)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Text("Cropped Image")
+                        Image(uiImage: croppedImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: screenWidth * 0.6, height: screenHeight/5)
+                        
+                        HStack {
+                            Button (action : {
+                                showCropper = true
+                            }) {
+                                Text("Crop")
+                                    .padding(.all, 10)
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .shadow(color: .gray, radius: 1)
+                                    .padding(.top, 50)
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+            NavigationView {
+                ZStack {
+                    // This will navigate into cropper view
+                    NavigationLink(destination: CropperView(inputImage: inputimage, croppedImage: $croppedImage),isActive: $showCropper) { EmptyView() }
                     
-                    HStack {
-                        Button (action : {
-                            showCropper = true
-                        }) {
-                            Text("Crop")
-                                .padding(.all, 10)
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .shadow(color: .gray, radius: 1)
-                                .padding(.top, 50)
+                    VStack {
+                        Text("Origin Image")
+                        Image(uiImage: inputimage)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Text("Cropped Image")
+                        Image(uiImage: croppedImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: screenWidth * 0.6, height: screenHeight/5)
+                        
+                        HStack {
+                            Button (action : {
+                                showCropper = true
+                            }) {
+                                Text("Crop")
+                                    .padding(.all, 10)
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .shadow(color: .gray, radius: 1)
+                                    .padding(.top, 50)
+                            }
                         }
                     }
                 }
